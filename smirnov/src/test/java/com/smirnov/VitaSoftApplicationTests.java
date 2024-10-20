8e1e2e2ad6dfd4a57209cb88abfb08d6542fb934
Dmitry <smirnoff.spb78@yandex.ru>
2024-10-20 12:09:06 +0300

    Создан проект

---
+++ smirnov/src/test/java/com/smirnov/VitaSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smirnov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
